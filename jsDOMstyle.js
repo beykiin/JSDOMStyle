9e694ae527a9b56426df7e6b59b0a242c2526fa2
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 16:47:19 +0300

    First Commit

---
+++ jsDOMstyle.js
@@ -0,0 +1,9 @@
+let box=document.querySelector(".box")
+box.style.height="100px"
+box.style.backgroundColor="black"
+box.style.width="100px"
+box.style.border="5px solid orangered"
+var btn=document.getElementById("btn")
+btn.innerHTML="Tamam"
+btn.style.borderRadius="10px"
+btn.style.marginTop="20px"
